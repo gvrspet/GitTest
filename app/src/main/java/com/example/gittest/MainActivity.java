5300dcb635887367711346cd6cee0461a9fb45b1
gvrspet <gvrspet@users.noreply.github.com>
2019-04-08 19:02:02 +0300

    Added one more activity

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    AppCompatButton nextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+
+    }
+    private void initComponents()
+    {
+        nextButton=(AppCompatButton)findViewById(R.id.button_next);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            }
+        });
     }
 }
